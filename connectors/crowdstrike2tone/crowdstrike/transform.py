f3963cd7c112c6c4603195db8e10db347181f9ac
Steven McGrath <smcgrath@tenable.com>
2024-12-13 17:28:41 -0600

    Updated the connector job declarations (#11)

--- connectors/crowdstrike2tone/crowdstrike/transform.py
+++ connectors/crowdstrike2tone/crowdstrike/transform.py
@@ -3,10 +3,9 @@ import logging
 from typing import Any
 
 import arrow
+from crowdstrike import CrowdStrikeAPI
 from restfly.utils import trunc
 from tenable.io import TenableIO
-
-from crowdstrike import CrowdStrikeAPI
 
 
 class Transformer:
@@ -18,18 +17,18 @@ class Transformer:
         """ """
         self.tvm = tvm if tvm else TenableIO()
         self.crwd = crwd if crwd else CrowdStrikeAPI()
-        self.log = logging.getLogger("Transformer")
+        self.log = logging.getLogger('Transformer')
 
     def run(self, get_findings: bool = True, last_seen_days: int = 1) -> None:
         with self.tvm.sync.create(
-            sync_id="tenable_crowdstrike_falcon",
-            vendor="tenable",
-            sensor="crowdstrike",
+            sync_id='tenable_crowdstrike_falcon',
+            vendor='crowd-strike',
+            sensor='falcon',
         ) as job:
             for asset in self.crwd.assets.list(last_seen_days=last_seen_days):
                 t1asset = self.transform_asset(asset)
-                self.log.debug("Adding asset id=%s to the job" % t1asset["id"])
-                job.add(t1asset, object_type="device-asset")
+                self.log.debug('Adding asset id=%s to the job' % t1asset['id'])
+                job.add(t1asset, object_type='device-asset')
             if not get_findings:
                 return
             raise NotImplementedError()
@@ -46,60 +45,60 @@ class Transformer:
         Attempts to derive the system type as required by the T1 Sync API.  If unsuccessful
         then we will return 'UNKNOWN'.
         """
-        type_map = {"windows": "WINDOWS", "linux": "LINUX", "macos": "MAC_OS"}
+        type_map = {'windows': 'WINDOWS', 'linux': 'LINUX', 'macos': 'MAC_OS'}
         for key, value in type_map.items():
             if system_type.lower() in key:
                 return value
-        return "UNKNOWN"
+        return 'UNKNOWN'
 
     def transform_asset(self, asset: dict[str, Any]) -> dict[str, Any]:
         """
         Converts a CS machine into a T1 compatable asset format.
         """
-        ip_map = {"IPv4Address": "ip_addresses_v4", "IPv6Address": "ip_addresses_v6"}
+        ip_map = {'IPv4Address': 'ip_addresses_v4', 'IPv6Address': 'ip_addresses_v6'}
         ret = {
-            "object_type": "device-asset",
-            "id": asset["device_id"],
-            "name": asset.get("hostname", None),
-            "device": {
-                "networking": {
-                    "mac_addresses": [asset.get("mac_address")]
-                    if asset.get("mac_address")
+            'object_type': 'device-asset',
+            'id': asset['device_id'],
+            'name': asset.get('hostname', None),
+            'device': {
+                'networking': {
+                    'mac_addresses': [asset.get('mac_address')]
+                    if asset.get('mac_address')
                     else None,
-                    "ip_addresses_v4": [],
-                    "ip_addresses_v6": [],
+                    'ip_addresses_v4': [],
+                    'ip_addresses_v6': [],
                 },
-                "hardware": {
-                    "serial_number": asset.get("serial_number"),
-                    "bios": {
-                        "manufacturer": asset.get("bios_manufacturer", None),
-                        "version": trunc(asset.get("bios_version"), 32)
-                        if asset.get("bios_version")
+                'hardware': {
+                    'serial_number': asset.get('serial_number'),
+                    'bios': {
+                        'manufacturer': asset.get('bios_manufacturer', None),
+                        'version': trunc(asset.get('bios_version'), 32)
+                        if asset.get('bios_version')
                         else None,
                     },
                 },
-                "operating_system": {
-                    "type": self.derive_system_type(asset.get("platform_name", "")),
+                'operating_system': {
+                    'type': self.derive_system_type(asset.get('platform_name', '')),
                 },
             },
-            "labels": asset.get("tags"),
-            "discovery": {
-                "first_observed_at": arrow.get(asset["first_seen"]).datetime,
-                "last_observed_on": arrow.get(asset["last_seen"]).datetime,
+            'labels': asset.get('tags'),
+            'discovery': {
+                'first_observed_at': arrow.get(asset['first_seen']).datetime,
+                'last_observed_on': arrow.get(asset['last_seen']).datetime,
             },
         }
-        for key in ("external_ip", "local_ip"):
+        for key in ('external_ip', 'local_ip'):
             value = asset.get(key)
             if value:
                 try:
                     ip = ipaddress.ip_address(value)
                 except ValueError:
-                    self.log.debug(f"{ip} is not a valid IP Address")
+                    self.log.debug(f'{ip} is not a valid IP Address')
                 else:
                     cname = ip.__class__.__name__
-                    obj = {"address": str(ip)}
-                    if obj not in ret["device"]["networking"][ip_map[cname]]:
-                        ret["device"]["networking"][ip_map[cname]].append(obj)
+                    obj = {'address': str(ip)}
+                    if obj not in ret['device']['networking'][ip_map[cname]]:
+                        ret['device']['networking'][ip_map[cname]].append(obj)
 
         #        if 'external_ip' in asset or 'local_ip' in asset:
         #            ipv4s = []

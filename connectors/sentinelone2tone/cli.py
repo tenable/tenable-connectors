ef883e8483624e84dd95c3fc8d6de6f6c0e10a4a
Steve McGrath <steve@mcgrath.sh>
2025-01-03 15:00:39 -0600

    Inititial Commit of SentinelOne Connector

---
+++ connectors/sentinelone2tone/cli.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import logging
+from enum import Enum
+
+from rich.logging import RichHandler
+from tenable.io import TenableIO
+from typer import Option, Typer
+from typing_extensions import Annotated
+
+from sentinelone.api.session import SentinelOneAPI
+from sentinelone.transform import Transformer
+
+app = Typer()
+
+
+class LogLevels(str, Enum):
+    debug = 'DEBUG'
+    info = 'INFO'
+    warn = 'WARN'
+    error = 'ERROR'
+
+
+def setup_logging(log_level: LogLevels) -> None:
+    """
+    Setup logging for s1 integration
+    """
+    fileHandler = logging.FileHandler('sentinelone2tone.log')
+    fileHandler.setFormatter(
+        logging.Formatter(
+            fmt='%(asctime)s %(levelname)-5.5s  %(message)s', datefmt='[%X]'
+        )
+    )
+    logging.basicConfig(
+        level=log_level.value,
+        format='%(message)s',
+        datefmt='[%X]',
+        handlers=[RichHandler(rich_tracebacks=True), fileHandler],
+    )
+
+
+@app.command()
+def run(
+    tio_access_key: Annotated[
+        str, Option(envvar='TIO_ACCESS_KEY', prompt=True, help='TVM/TIO API Access Key')
+    ],
+    tio_secret_key: Annotated[
+        str, Option(envvar='TIO_SECRET_KEY', prompt=True, help='TVM/TIO API Secret Key')
+    ],
+    s1_api_token: Annotated[
+        str, Option(envvar='S1_API_TOKEN', prompt=True, help='SentienlOne API Token')
+    ],
+    s1_url: Annotated[
+        str, Option(envvar='S1_URL', prompt=True, help='SentinelOne URL')
+    ],
+    tio_url: Annotated[
+        str, Option(envvar='TIO_URL', help='TVM/TIO URL')
+    ] = 'https://cloud.tenable.com',
+    log_level: Annotated[
+        LogLevels, Option(envvar='LOG_LEVEL', help='Output logging level')
+    ] = 'INFO',
+    download_vulns: Annotated[
+        bool, Option(help='Import the SentinelOne Vulndreability Findings')
+    ] = True,
+) -> None:
+    """
+    Run the Sentinel One connector
+    """
+    setup_logging(log_level)
+
+    tvm = TenableIO(access_key=tio_access_key, secret_key=tio_secret_key, url=tio_url)
+    s1 = SentinelOneAPI(api_token=s1_api_token, url=s1_url)
+    d2t1 = Transformer(tvm=tvm, s1=s1)
+    d2t1.run(get_findings=download_vulns)
+
+
+if __name__ == '__main__':
+    app()
---
+++ connectors/sentinelone2tone/sentinelone/api/session.py
@@ -0,0 +1,50 @@
+import os
+from restfly.session import APISession
+from sentinelone.api.findings import Findings
+from sentinelone.api.assets import Assets
+
+class SentinelOneAPI(APISession):
+    """
+    
+    Docs:
+        https://usea1-partners.sentinelone.net/api-doc/overview
+    """
+    
+    
+    _base_path = 'web/api/v2.1'
+    _box = True
+    
+    def __init__(self, **kwargs):
+        """
+        Initialize the SentinelOne API Session.
+        
+        Args:
+            url: The customer API url
+            api_token: The Customer instance URL
+        """
+        params = (
+            ('url', os.environ.get('S1_URL')),
+            ('api_token', os.environ.get('S1_API_TOKEN')),
+        )
+        for key, envval in params:
+            if envval and not kwargs.get(key):
+                kwargs[key] = envval
+        if not kwargs.get('url'):
+            raise ConnectionError('No valid url provided')
+        if not kwargs.get('api_token'):
+            raise ConnectionError('No valid api_token was not provided.')
+        super().__init__(**kwargs)
+        
+    def _authenticate(self, api_token):
+        self._session.headers = {
+            'Authorization' : f'ApiToken {api_token}',
+            'Content-Type': 'application/json'
+        }
+    
+    @property
+    def assets(self):
+        return Assets(self)
+    
+    @property
+    def findings(self):
+        return Findings(self)
